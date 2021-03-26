b5ff6fb6f8585729371a0410080bf5dbb74d2c08
saidfernando <saidfer83@gmail.com>
2021-03-26 09:48:15 -0500

    26_03_2021

---
+++ main.js
@@ -0,0 +1,22 @@
+
+
+let nombre, apellido;
+const edad = 35;
+nombre = "said";
+apellido = "rios";
+console.log(nombre, apellido, edad);
+
+
+// let nombre = "said fernando rios"; //string
+// let edad = 35; // nomber integer
+// let altura = 1.65;  // doble float
+// let profesor = true; //boolen
+// let hobbis = [nombre, edad, altura, profesor];
+// //console.log(informacion);
+// let vaso = {
+//     material : "porsrlana",
+//     diametro : 60,
+//     lleno : falce,
+// };
+// console.log(voso);
+// //console.log(voso.diametro);
